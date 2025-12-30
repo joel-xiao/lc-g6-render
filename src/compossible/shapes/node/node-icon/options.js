57442200f08ec8624f314f1d72c01ab94b2fcd88
肖文龙 <joel.xiao@dclingcloud.com>
2025-12-30 15:02:08 +0800

    fix: change node-icon options to default export

--- src/compossible/shapes/node/node-icon/options.js
+++ src/compossible/shapes/node/node-icon/options.js
@@ -1,26 +1,18 @@
-export function getDefaultNode(type) {
-    const defaultData = {
-        'node-icon': {
-            type: "node-icon",
-            statusType: 'normal',
-            shape: 'hexagonal-polygon',
-            size: 70,
-            style: {
-                lineWidth: 3
+lineWidth: 3
             },
-            center: {
-                show: true,
-                text: undefined
+center: {
+    show: true,
+        text: undefined
+},
+
+rightTop: {
+    show: true,
             },
 
-            rightTop: {
-                show: true,
-            },
-
-            rightBottom: {
-                show: true,
+rightBottom: {
+    show: true,
             }
         },
     }
-    return defaultData[type || 'node-icon'];
+return defaultData[type || 'node-icon'];
 }
