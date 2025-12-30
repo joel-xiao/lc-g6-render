13222129ba96985d788f303d9e0b2ea1d2a66a6b
肖文龙 <joel.xiao@dclingcloud.com>
2025-12-30 14:15:51 +0800

    refactor: consolidate registries into core/registry.js

--- src/compossible/core/registry.js
+++ src/compossible/core/registry.js
@@ -1,8 +1,83 @@
-import { nodeRegistry } from '../registry/node-registry';
-import { edgeRegistry } from '../registry/edge-registry';
-import { comboRegistry } from '../registry/combo-registry';
-import { layoutRegistry } from '../registry/layout-registry';
-// import { behaviorRegistry } from '../registry/behavior-registry'; // Future
+import G6 from "@antv/g6";
+import { getRegisterNode, getRegisterEdge, getRegisterCombo } from '../shapes/index';
+import { registerLayout as dagreTgb } from '../layouts/dagre-tgb/index';
+import { registerLayout as dagreTbt } from '../layouts/dagre-tbt/index';
+
+const registerLayoutDef = {
+    ...dagreTgb,
+    ...dagreTbt
+};
+
+class NodeRegistry {
+    constructor() {
+        this.definitions = {};
+    }
+
+    register(g6_example) {
+        const definitions = getRegisterNode(g6_example);
+        Object.keys(definitions).forEach(key => {
+            G6.registerNode(key, definitions[key].node, definitions[key].register);
+            this.definitions[key] = definitions[key];
+        });
+    }
+
+    get(type) {
+        return this.definitions[type];
+    }
+}
+
+class EdgeRegistry {
+    constructor() {
+        this.definitions = {};
+    }
+
+    register(g6_example) {
+        const definitions = getRegisterEdge(g6_example);
+        Object.keys(definitions).forEach(key => {
+            G6.registerEdge(key, definitions[key].edge, definitions[key].register);
+            this.definitions[key] = definitions[key];
+        });
+    }
+
+    get(type) {
+        return this.definitions[type];
+    }
+}
+
+class ComboRegistry {
+    constructor() {
+        this.definitions = {};
+    }
+
+    register(g6_example) {
+        const definitions = getRegisterCombo(g6_example);
+        Object.keys(definitions).forEach(key => {
+            G6.registerCombo(key, definitions[key].combo, definitions[key].register);
+            this.definitions[key] = definitions[key];
+        });
+    }
+
+    get(type) {
+        return this.definitions[type];
+    }
+}
+
+class LayoutRegistry {
+    constructor() {
+        this.definitions = {};
+    }
+
+    register() {
+        Object.keys(registerLayoutDef).forEach(key => {
+            G6.registerLayout(key, registerLayoutDef[key].layout, registerLayoutDef[key].register);
+            this.definitions[key] = registerLayoutDef[key];
+        });
+    }
+
+    get(type) {
+        return this.definitions[type];
+    }
+}
 
 /**
  * Core Registry Manager
@@ -12,11 +87,10 @@ class Registry {
     constructor() {
         this.registered = false;
         this.registries = {
-            node: nodeRegistry,
-            edge: edgeRegistry,
-            combo: comboRegistry,
-            layout: layoutRegistry,
-            // behavior: behaviorRegistry
+            node: new NodeRegistry(),
+            edge: new EdgeRegistry(),
+            combo: new ComboRegistry(),
+            layout: new LayoutRegistry(),
         };
     }
 
@@ -41,6 +115,11 @@ class Registry {
     }
 
     // Accessors for specific registries
+    get node() { return this.registries.node; }
+    get edge() { return this.registries.edge; }
+    get combo() { return this.registries.combo; }
+    get layout() { return this.registries.layout; }
+
     getNode(type) { return this.registries.node.get(type); }
     getEdge(type) { return this.registries.edge.get(type); }
     getCombo(type) { return this.registries.combo.get(type); }
