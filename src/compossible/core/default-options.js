f1870bfe8bbd4988f15616c3a4a163e5e7f8cad0
肖文龙 <joel.xiao@dclingcloud.com>
2025-12-30 13:04:02 +0800

    refactor: optimize behaviors structure and modes config

--- src/compossible/core/default-options.js
+++ src/compossible/core/default-options.js
@@ -1,8 +1,4 @@
-import { getModels } from '../behaviors/modes';
-import { getLayout } from '../layouts/options';
-import { getDefaultNode } from '../shapes/node/defaults';
-import { getDefaultEdge } from '../shapes/edge/defaults';
-import { getDefaultCombo } from '../shapes/combo/defaults';
+import { getModels, getLayout, getDefaultNode, getDefaultEdge, getDefaultCombo } from '../graph-options';
 
 // 临时导入旧的配置生成函数，后续重构时会移除这些依赖
 
